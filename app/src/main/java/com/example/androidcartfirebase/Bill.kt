682cb8c885d1ef060c13baf1b4039bc126e9cbf3
IT21232640 <it21232640@my.sliit.lk>
2023-05-06 20:24:05 +0530

    init

---
+++ app/src/main/java/com/example/androidcartfirebase/Bill.kt
@@ -0,0 +1,72 @@
+package com.example.androidcartfirebase
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import com.google.firebase.database.*
+
+class Bill : AppCompatActivity() {
+
+    private lateinit var etBBtnOk: Button
+    private lateinit var dbRef: DatabaseReference
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bill)
+        val paymentId = intent.getStringExtra("paymentId") ?: ""
+        if (paymentId.isBlank()) {
+            Toast.makeText(this, "Payment ID not found", Toast.LENGTH_SHORT).show()
+            finish()
+            return
+        }
+
+        dbRef = FirebaseDatabase.getInstance().getReference("Payments").child(paymentId)
+
+        val etBGetPaymentID: EditText = findViewById(R.id.etBGetPaymentID)
+//        val etBGetCardNumber: EditText = findViewById(R.id.etBGetCardNumber)
+        val etBGetAmount: EditText = findViewById(R.id.etBGetAmount)
+        val etBGetTotAmount: EditText = findViewById(R.id.etBGetTotAmount)
+        val etBGetDDate: EditText = findViewById(R.id.etBGetDDate)
+        etBBtnOk = findViewById(R.id.etBBtnOk)
+
+        etBBtnOk.setOnClickListener {
+            val intent = Intent(this@Bill, Payment::class.java)
+            startActivity(intent)
+        }
+
+        dbRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    val payment = snapshot.getValue(PaymentModel::class.java)
+                    if (payment != null) {
+                        etBGetPaymentID.setText(payment.paymentId)
+//                        etBGetCardNumber.setText(payment.cardNum)
+                        etBGetAmount.setText(payment.amount)
+                        etBGetTotAmount.setText(payment.totalAmount)
+                        etBGetDDate.setText(payment.deliveryDate)
+                    } else {
+                        Toast.makeText(this@Bill, "Payment data not found", Toast.LENGTH_SHORT).show()
+                        finish()
+                    }
+                } else {
+                    Toast.makeText(this@Bill, "Payment ID not found", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@Bill, "Error: ${error.message}", Toast.LENGTH_SHORT).show()
+                Log.e("Bill", "Firebase error", error.toException())
+                finish()
+            }
+        })
+    }
+}
---
+++ app/src/main/java/com/example/androidcartfirebase/Login.kt
@@ -0,0 +1,88 @@
+package com.example.androidcartfirebase
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+class Login : AppCompatActivity() {
+
+    private lateinit var etLEmail: EditText
+    private lateinit var etLPassword: EditText
+    private lateinit var etLBtnLogin: Button
+    private lateinit var etLBtnSignup: Button
+
+    private lateinit var dbRef: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        etLEmail = findViewById(R.id.etLEmail)
+        etLPassword = findViewById(R.id.etLPassword)
+        etLBtnLogin = findViewById(R.id.etLBtnLogin)
+        etLBtnSignup = findViewById(R.id.etLBtnSignup)
+
+        dbRef = FirebaseDatabase.getInstance().getReference("Customers")
+
+        etLBtnSignup.setOnClickListener {
+            val intent = Intent(this@Login, Registration::class.java)
+            startActivity(intent)
+        }
+
+        etLBtnLogin.setOnClickListener {
+            val email = etLEmail.text.toString()
+            val password = etLPassword.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please enter Email and Password", Toast.LENGTH_LONG).show()
+            } else {
+                dbRef.orderByChild("email").equalTo(email)
+                    .addListenerForSingleValueEvent(object : ValueEventListener {
+                        override fun onDataChange(dataSnapshot: DataSnapshot) {
+                            if (dataSnapshot.exists()) {
+                                for (userSnapshot in dataSnapshot.children) {
+                                    val user = userSnapshot.getValue(RegistrationModel::class.java)
+                                    if (user?.password == password) {
+                                        val intent = Intent(this@Login, MainActivity::class.java)
+                                        intent.putExtra("cusId", user?.cusId)
+                                        startActivity(intent)
+                                        finish()
+                                    } else {
+                                        Toast.makeText(
+                                            this@Login,
+                                            "Invalid Password",
+                                            Toast.LENGTH_LONG
+                                        ).show()
+                                    }
+                                }
+                            } else {
+                                Toast.makeText(
+                                    this@Login,
+                                    "User not found",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            }
+                        }
+
+                        override fun onCancelled(databaseError: DatabaseError) {
+                            Toast.makeText(
+                                this@Login,
+                                "Error ${databaseError.message}",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    })
+            }
+        }
+    }
+}
